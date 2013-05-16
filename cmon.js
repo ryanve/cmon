80997183d9f5adc46c1a192b9c116b930a87c190
Ryan Van Etten <ryanve@gmail.com>
2013-05-16 01:16:03 -0400

    0.2.0: Encapsulate event methods. Remove their top-level aliases.

--- cmon.js
+++ cmon.js
@@ -2,7 +2,7 @@
  * @link        github.com/ryanve/cmon
  * @license     MIT
  * @copyright   2013 Ryan Van Etten
- * @version     0.1.6
+ * @version     0.2.0
  */
 
 /*jshint expr:true, laxcomma:true, supernew:true, debug:true, eqnull:true, node:true, boss:true, evil:true,
@@ -21,11 +21,43 @@
 }(this, 'cmon', function() {
 
     var root = this || window
-      , handlers = {}
       , modules = {}
       , claimed = {}
       , owns = claimed.hasOwnProperty;
 
+    /**
+     * @param  {string|number} id
+     * @link   wiki.commonjs.org/wiki/Modules/1.1.1
+     */
+    function require(id) {
+        if (null == id) throw new TypeError('@require');
+        return (owns.call(modules, id) ? modules : root)[id];
+    }
+    require['main'] = void 0;
+
+    /**
+     * @param  {string|number} id
+     * @param  {*=}            value
+     */
+    function provide(id, value) {
+        if (null == id) throw new TypeError('@provide');
+        modules[id] = value;
+        provide['trigger'](id);
+        return value;
+    }
+
+    /**
+     * @param  {string|number|Function} id
+     * @param  {*=}  value
+     */
+    function cmon(id, value) {
+        if (typeof id != 'function')
+            // Check for 2 so that arrays map v/i/a as require
+            return 2 == arguments.length ? provide.call(root, id, value) : require.call(root, id);
+        // Call callback and return undefined
+        id.call(root, cmon);
+    }
+    
     /**
      * @param  {string|number} id
      * @param  {*=}            value
@@ -52,82 +84,6 @@
     }
     
     /**
-     * @param  {Array|Object} fns
-     * @param  {*=}           scope
-     */
-    function callEach(fns, scope) {
-        if (!fns) { return; }
-        for (var i = 0, l = fns.length; i < l;) {
-            if (fns[i++].call(scope) === false) {
-                break;
-            }
-        }
-    }
-
-    /**
-     * @param  {string|number} id
-     * @link   wiki.commonjs.org/wiki/Modules/1.1.1
-     */
-    function require(id) {
-        if (null == id) throw new TypeError('@require');
-        return (owns.call(modules, id) ? modules : root)[id];
-    }
-    require['main'] = void 0;
-    
-    /**
-     * @param  {string|number} id
-     * @param  {*=}            value
-     */
-    function provide(id, value) {
-        if (null == id) throw new TypeError('@provide');
-        modules[id] = value;
-        owns.call(handlers, id) && callEach(handlers[id], root); // trigger
-        return value;
-    }
-
-    /**
-     * @param  {string|number|Function} id
-     * @param  {*=}  value
-     */
-    function cmon(id, value) {
-        if (typeof id != 'function')
-            // Check for 2 so that arrays map v/i/a as require
-            return 2 == arguments.length ? provide.call(root, id, value) : require.call(root, id);
-        // Call callback and return undefined
-        id.call(root, cmon);
-    }
-
-    /**
-     * @param  {string|number} id
-     * @param  {Function}      fn
-     * @return {number}
-     */    
-    function on(id, fn) {
-        if (null == id || typeof fn != 'function') throw new TypeError('@on');
-        return (handlers[id] = owns.call(handlers, id) && handlers[id] || []).push(fn);
-    }
-
-    /**
-     * @param  {string|number} id
-     * @param  {Function=}     fn
-     * @return {number}
-     */
-    function off(id, fn) {
-        var fns, i;
-        if (null != id) {
-            if (void 0 === fn) {
-                handlers[id] = fn; // undefine (remove all)
-            } else if (fns = owns.call(handlers, id) && handlers[id]) {
-                for (i = fns.length; i--;) {
-                    fn === fns[i] && fns.splice(i, 1);
-                }
-                return fns.length;
-            }
-        }
-        return 0;
-    }
-    
-    /**
      * @this   {Object|Function}
      * @param  {(boolean|Function)=} fn 
      * @return {Object|Function}
@@ -139,9 +95,63 @@
         typeof fn == 'function' && fn.call(root, this); 
         return this;
     }
+    
+    // Make an on/off/trigger event API for provide()
+    (function(target, triggerScope, handlers, owns) {
 
-    cmon['on'] = provide['on'] = on;
-    cmon['off'] = provide['off'] = off;
+        /**
+         * @param  {Array|Object} fns
+         * @param  {*=}           scope
+         */
+        function callEach(fns, scope) {
+            if (!fns) { return; }
+            for (var i = 0, l = fns.length; i < l;) {
+                if (fns[i++].call(scope) === false) {
+                    break;
+                }
+            }
+        }
+        
+        /**
+         * @param  {string|number} id
+         */    
+        target['trigger'] = function(id) {
+            owns.call(handlers, id) && callEach(handlers[id], triggerScope);
+        };
+    
+        /**
+         * @param  {string|number} id
+         * @param  {Function}      fn
+         * @return {number}
+         */    
+        target['on'] = function(id, fn) {
+            if (null == id || typeof fn != 'function') throw new TypeError('@on');
+            return (handlers[id] = owns.call(handlers, id) && handlers[id] || []).push(fn);
+        };
+        
+        /**
+         * @param  {string|number} id
+         * @param  {Function=}     fn
+         * @return {number}
+         */
+        target['off'] = function(id, fn) {
+            var fns, i;
+            if (null != id) {
+                if (void 0 === fn) {
+                    handlers[id] = fn; // undefine (remove all)
+                } else if (fns = owns.call(handlers, id) && handlers[id]) {
+                    for (i = fns.length; i--;) {
+                        fn === fns[i] && fns.splice(i, 1);
+                    }
+                    return fns.length;
+                }
+            }
+            return 0;
+        };
+        
+        return target;
+    }(provide, root, {}, owns));
+
     cmon['provide'] = provide;
     cmon['require'] = require;
     cmon['claim'] = claim;
